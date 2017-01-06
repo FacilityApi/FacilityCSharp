ecfdd184f00e830b3d1efe1efd0fefd79faddfb5
Ed Ball <ed.ball@logos.com>
2017-01-06 09:22:48 -0800

    Update copyright.

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyVersion("0.3.0.0")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright 2017 Ed Ball")]
+[assembly: AssemblyCopyright("Copyright 2016-2017 Ed Ball")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
