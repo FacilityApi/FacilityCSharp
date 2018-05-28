addf66d88668c8c10d2462b5402f0756375b45bb
Ed Ball <ejball@gmail.com>
2018-05-28 10:00:00 -0700

    Refactor into CommonClientAspects.

---
+++ src/Facility.Core/Http/CommonClientAspects.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Facility.Core.Http
+{
+	/// <summary>
+	/// Common implementations of <see cref="HttpClientServiceAspect"/>.
+	/// </summary>
+	public static class CommonClientAspects
+	{
+		/// <summary>
+		/// Sets the Accept header of the request.
+		/// </summary>
+		public static HttpClientServiceAspect RequestAccept(string accept) => RequestHeader("Accept", accept);
+
+		/// <summary>
+		/// Sets the Authorization header of the request.
+		/// </summary>
+		public static HttpClientServiceAspect RequestAuthorization(string authorization) => RequestHeader("Authorization", authorization);
+
+		/// <summary>
+		/// Sets a request header.
+		/// </summary>
+		public static HttpClientServiceAspect RequestHeader(string headerName, string headerValue)
+		{
+			if (string.IsNullOrWhiteSpace(headerName))
+				throw new ArgumentException("Invalid header name.", nameof(headerName));
+
+			return RequestReady(request =>
+			{
+				if (!string.IsNullOrEmpty(headerValue))
+					request.Headers.Add(headerName, headerValue);
+			});
+		}
+
+		/// <summary>
+		/// Executes the action when the request is ready.
+		/// </summary>
+		public static HttpClientServiceAspect RequestReady(Action<HttpRequestMessage> action) => new RequestReadyClientAspect(action);
+
+		/// <summary>
+		/// Sets the User-Agent header of the request.
+		/// </summary>
+		public static HttpClientServiceAspect RequestUserAgent(string userAgent) => RequestHeader("User-Agent", userAgent);
+
+		private sealed class RequestReadyClientAspect : HttpClientServiceAspect
+		{
+			public RequestReadyClientAspect(Action<HttpRequestMessage> action)
+			{
+				m_action = action ?? throw new ArgumentNullException(nameof(action));
+			}
+
+			protected override Task RequestReadyAsyncCore(HttpRequestMessage request, ServiceDto requestDto, CancellationToken cancellationToken)
+			{
+				m_action(request);
+				return s_completedTask;
+			}
+
+			private readonly Action<HttpRequestMessage> m_action;
+		}
+
+		private static readonly Task<object> s_completedTask = Task.FromResult<object>(null);
+	}
+}
---
+++ tests/Facility.Core.UnitTests/Http/CommonClientAspectsTests.cs
@@ -0,0 +1,62 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Facility.Core.Http;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Facility.Core.UnitTests.Http
+{
+	[SuppressMessage("ReSharper", "ConsiderUsingConfigureAwait", Justification = "unit tests")]
+	public class CommonClientAspectsTests
+	{
+		[Test]
+		public async Task RequestAccept()
+		{
+			const string header = HttpServiceUtility.JsonMediaType;
+			var aspect = CommonClientAspects.RequestAccept(header);
+			var httpRequest = new HttpRequestMessage();
+			await aspect.RequestReadyAsync(httpRequest, new TestDto(), CancellationToken.None);
+			httpRequest.Headers.Accept.ToString().Should().Be(header);
+		}
+
+		[Test]
+		public async Task RequestAuthorization()
+		{
+			const string header = "MyAuth Whatever";
+			var aspect = CommonClientAspects.RequestAuthorization(header);
+			var httpRequest = new HttpRequestMessage();
+			await aspect.RequestReadyAsync(httpRequest, new TestDto(), CancellationToken.None);
+			httpRequest.Headers.Authorization.ToString().Should().Be(header);
+		}
+
+		[Test]
+		public async Task RequestHeader()
+		{
+			const string headerName = "X-Adjective";
+			const string headerValue = "shiny";
+			var aspect = CommonClientAspects.RequestHeader(headerName, headerValue);
+			var httpRequest = new HttpRequestMessage();
+			await aspect.RequestReadyAsync(httpRequest, new TestDto(), CancellationToken.None);
+			httpRequest.Headers.TryGetValues(headerName, out var values);
+			values.Single().Should().Be(headerValue);
+		}
+
+		[Test]
+		public async Task RequestUserAgent()
+		{
+			const string header = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36";
+			var aspect = CommonClientAspects.RequestUserAgent(header);
+			var httpRequest = new HttpRequestMessage();
+			await aspect.RequestReadyAsync(httpRequest, new TestDto(), CancellationToken.None);
+			httpRequest.Headers.UserAgent.ToString().Should().Be(header);
+		}
+
+		private class TestDto : ServiceDto<TestDto>
+		{
+			public override bool IsEquivalentTo(TestDto other) => other != null;
+		}
+	}
+}
