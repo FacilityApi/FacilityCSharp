817e2ff40a2112fafadf930d9700b9de5d6a28cd
Ed Ball <ejball@gmail.com>
2018-06-09 09:55:01 -0700

    Improve test coverage.

--- tests/Facility.CodeGen.CSharp.UnitTests/CSharpGeneratorTests.cs
+++ tests/Facility.CodeGen.CSharp.UnitTests/CSharpGeneratorTests.cs
@@ -12,13 +12,41 @@ namespace Facility.CodeGen.CSharp.UnitTests
 		[Test]
 		public void DuplicateType()
 		{
-			var parser = new FsdParser();
-			var service = parser.ParseDefinition(new ServiceDefinitionText("TestApi.fsd",
-				"service TestApi { method do {}: {} data doRequest {} }"));
-			var generator = new CSharpGenerator { GeneratorName = "CodeGenTests" };
-			Action action = () => generator.GenerateOutput(service);
-			action.Should().Throw<ServiceDefinitionException>()
-				.WithMessage("TestApi.fsd(1,36): Element generates duplicate C# type 'DoRequestDto'.");
+			ThrowsServiceDefinitionException(
+				"service TestApi { method do {}: {} data doRequest {} }",
+				"TestApi.fsd(1,36): Element generates duplicate C# type 'DoRequestDto'.");
+		}
+
+		[Test]
+		public void DuplicateServiceAttribute()
+		{
+			ThrowsServiceDefinitionException(
+				"[csharp] [csharp] service TestApi { method do {}: {} }",
+				"TestApi.fsd(1,11): 'csharp' attribute is duplicated.");
+		}
+
+		[Test]
+		public void UnknownServiceAttributeParameter()
+		{
+			ThrowsServiceDefinitionException(
+				"[csharp(name: hmm)] service TestApi { method do {}: {} }",
+				"TestApi.fsd(1,9): Unexpected 'csharp' parameter 'name'.");
+		}
+
+		[Test]
+		public void UnknownMethodAttribute()
+		{
+			ThrowsServiceDefinitionException(
+				"service TestApi { [csharp] method do {}: {} }",
+				"TestApi.fsd(1,20): Unexpected 'csharp' attribute.");
+		}
+
+		[Test]
+		public void UnknownFieldAttributeParameter()
+		{
+			ThrowsServiceDefinitionException(
+				"service TestApi { method do { [csharp(namespace: hmm)] something: string; }: {} }",
+				"TestApi.fsd(1,39): Unexpected 'csharp' parameter 'namespace'.");
 		}
 
 		[Test]
@@ -31,6 +59,15 @@ namespace Facility.CodeGen.CSharp.UnitTests
 
 			var generator = new CSharpGenerator { GeneratorName = nameof(CSharpGeneratorTests) };
 			generator.GenerateOutput(service);
+		}
+
+		private void ThrowsServiceDefinitionException(string definition, string message)
+		{
+			var parser = new FsdParser();
+			var service = parser.ParseDefinition(new ServiceDefinitionText("TestApi.fsd", definition));
+			var generator = new CSharpGenerator { GeneratorName = "CodeGenTests" };
+			Action action = () => generator.GenerateOutput(service);
+			action.Should().Throw<ServiceDefinitionException>().WithMessage(message);
 		}
 	}
 }
